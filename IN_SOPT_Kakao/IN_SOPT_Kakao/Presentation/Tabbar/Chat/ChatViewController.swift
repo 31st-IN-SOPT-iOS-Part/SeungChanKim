66e645dd91eee61ece9b20d914fc9b00b720e2fd
seungchan <chanchu@hansung.ac.kr>
2022-10-13 22:14:11 +0900

    [#7] make ViewController

---
+++ IN_SOPT_Kakao/IN_SOPT_Kakao/Presentation/Tabbar/Chat/ChatViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ChatViewController.swift
+//  IN_SOPT_Kakao
+//
+//  Created by 김승찬 on 2022/10/12.
+//
+
+import UIKit
+
+final class ChatViewController: BaseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .red
+    }
+
+}
---
+++ IN_SOPT_Kakao/IN_SOPT_Kakao/Presentation/Tabbar/MyView/MyViewViewController.swift
@@ -0,0 +1,15 @@
+//
+//  MyViewViewController.swift
+//  IN_SOPT_Kakao
+//
+//  Created by 김승찬 on 2022/10/12.
+//
+
+import UIKit
+
+final class MyViewViewController: BaseViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
