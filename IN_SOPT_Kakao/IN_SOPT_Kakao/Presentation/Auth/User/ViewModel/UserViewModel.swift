334326f629ed04a1ae61c0377fc5dae90bbf08fa
seungchan <chanchu@hansung.ac.kr>
2022-10-13 22:13:04 +0900

    [#7] User

---
+++ IN_SOPT_Kakao/IN_SOPT_Kakao/Presentation/Auth/User/ViewModel/UserViewModel.swift
@@ -0,0 +1,46 @@
+//
+//  UserViewModel.swift
+//  IN_SOPT_Kakao
+//
+//  Created by 김승찬 on 2022/10/05.
+//
+
+import RxSwift
+import RxCocoa
+
+final class UserViewModel: ViewModelType {
+    
+    var disposeBag = DisposeBag()
+    
+    private weak var coordinator: SignUpCoordinator?
+    
+    init(coordinator: SignUpCoordinator?) {
+        self.coordinator = coordinator
+    }
+    
+    struct Input {
+        let pressConfirmButton: Signal<Void>
+    }
+    
+    struct Output {
+        let didConfirmButtonTapped: Signal<Void>
+        let userEmailText: Driver<String>
+    }
+    
+    private let didConfirmButtonTapped =  PublishRelay<Void>()
+    private let userEmailText = BehaviorRelay<String>(value: UserDefaultsManager.email)
+    
+    func transform(input: Input) -> Output {
+    
+        input.pressConfirmButton
+            .emit(onNext: { [weak self] in
+                guard let self = self else { return }
+                self.coordinator?.connectTabBarCoordinator()
+                self.didConfirmButtonTapped.accept(())
+            })
+            .disposed(by: disposeBag)
+        
+        return Output(didConfirmButtonTapped: didConfirmButtonTapped.asSignal(),
+                      userEmailText: userEmailText.asDriver())
+    }
+}
