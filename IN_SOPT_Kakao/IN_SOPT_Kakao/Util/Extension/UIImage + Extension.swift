0fb4382dff224bce51b7678e3b406f3487eb2d5c
seungchan <chanchu@hansung.ac.kr>
2022-11-04 14:14:25 +0900

    [#11] add Extension

---
+++ IN_SOPT_Kakao/IN_SOPT_Kakao/Util/Extension/UIImage + Extension.swift
@@ -0,0 +1,47 @@
+//
+//  UIImage + Extension.swift
+//  IN_SOPT_Kakao
+//
+//  Created by 김승찬 on 2022/10/28.
+//
+
+import UIKit
+
+extension UIImage {
+    
+    func resize(newWidth: CGFloat) -> UIImage {
+        let scale = newWidth / self.size.width
+        let newHeight = self.size.height * scale
+        
+        let size = CGSize(width: newWidth, height: newHeight)
+        let render = UIGraphicsImageRenderer(size: size)
+        let renderImage = render.image { context in
+            self.draw(in: CGRect(origin: .zero, size: size))
+        }
+        
+        print("화면 배율: \(UIScreen.main.scale)")
+        print("origin: \(self), resize: \(renderImage)")
+        
+        return renderImage
+    }
+    
+    func downSample2(size: CGSize, scale: CGFloat = UIScreen.main.scale) -> UIImage {
+        let imageSourceOption = [kCGImageSourceShouldCache: false] as CFDictionary
+        let data = self.pngData()! as CFData
+        let imageSource = CGImageSourceCreateWithData(data, imageSourceOption)!
+        
+        let maxPixel = max(size.width, size.height) * scale
+        let downSampleOptions = [
+            kCGImageSourceCreateThumbnailFromImageAlways: true,
+            kCGImageSourceShouldCacheImmediately: true,
+            kCGImageSourceCreateThumbnailWithTransform: true,
+            kCGImageSourceThumbnailMaxPixelSize: maxPixel
+        ] as CFDictionary
+        
+        let downSampledImage = CGImageSourceCreateThumbnailAtIndex(imageSource, 0, downSampleOptions)!
+        
+        let newImage = UIImage(cgImage: downSampledImage)
+        print(newImage)
+        return newImage
+    }
+}
